0a612d146a91f446b0a0836ab68969057b0e8c57
Seyidmammadli_i <Seyidmammadli_i@STHQ0123-01>
2023-10-24 11:45:31 +0400

    Add project files.

---
+++ LibraryProject.Data/Repositories/Concretes/Repository.cs
@@ -0,0 +1,61 @@
+﻿using LibraryProject.Data.Contexts;
+using LibraryProject.Data.Repositories.Abstracts;
+using LibraryProject.Models.Entities.Abstracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.Data.Repositories.Concretes
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity, new()
+    {
+        private readonly LibraryDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository()
+        {
+            _context=new LibraryDbContext();
+            _dbSet=_context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            if (entity==null) throw new ArgumentNullException("Data is null");
+            _dbSet.Add(entity);
+        }
+
+        public void AddRange(ICollection<T> entities)
+        {
+            if (_dbSet==null||entities.Count==0) throw new ArgumentNullException("Data in null");
+            _dbSet.AddRange(entities);
+        }
+
+        public ICollection<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChange()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LibraryProject.Models/Entities/Concretes/Book.cs
@@ -0,0 +1,26 @@
+﻿using LibraryProject.Models.Entities.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.Models.Entities.Concretes
+{
+    public class Book : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Pages { get; set; }
+        public int YearPress { get; set; }
+        public int IdThemes { get; set; }
+        public int IdCategory { get; set; }
+        public int IdAuthor { get; set; }
+        public int IdPress { get; set; }
+        public string? Comment { get; set; }
+        public int Quantity { get; set; }
+
+        public virtual Author IdAuthorNavigation { get; set; } = null!;
+        public virtual Category IdCategoryNavigation { get; set; } = null!;
+
+    }
+}
